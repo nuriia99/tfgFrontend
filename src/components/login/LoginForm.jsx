7d00fd996c4793c3d19dd4c3095c7aff947a80f9
nuria <nurianton1999@gmail.com>
2022-10-23 12:31:17 +0200

    login component

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const Navbar = () => {
   return (
       <div className="login">
-        Login
+        <div className="login_container">
+          <div className="login_container_title">
+           <h3>Iniciar sesión</h3>
+          </div>
+          
+          <div className="login_container_input">
+            <input type="text" name="" id="" required="required"/>
+            <span>Usuario</span>
+          </div>
+          <div className="login_container_input">
+            <input type="text" name="" id="" required="required"/>
+            <span>Contraseña</span>
+          </div>
+          <div className="login_container_button">
+           <button type="submit">Iniciar sesión</button>
+          </div>
+          
+        </div>
       </div>
   )
 }
